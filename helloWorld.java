cb5d84213d3f017793824aea4811e9f0f69ebd8d
Kirk Shrieves <kirkshrieves@gmail.com>
2022-10-27 20:49:21 -0700

    first commit

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,7 @@ package week01;
 public class helloWorld {
 
 	public static void main(String[] args) {
-		//git hub assignment - 2
+		//git hub assignment
 		System.out.println("Hello World!");
 
 	}
