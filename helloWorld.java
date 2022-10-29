a9ab6ea482608cff60bf163ce3a4da944c68b1c3
Kirk Shrieves <kirkshrieves@gmail.com>
2022-10-28 19:57:02 -0700

    second commit

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,7 @@ package week01;
 public class helloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// comment
 		System.out.println("Hello World!");
 	}
 
